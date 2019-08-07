13a6665fa4fd5a95560846e4e1cce838b1912458
KorayAGAYA <koray@korayagaya.com.tr>
2019-08-07 20:29:58 +0300

    Web

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Project(models.Model):
+    VulnerabilityID = models.CharField(max_length=10000)
+    PkgName = models.CharField(max_length=10000)
+    InstalledVersion = models.CharField(max_length=10000)
+    FixedVersion = models.CharField(max_length=10000)
+    Title = models.CharField(max_length=10000)
+    Description = models.CharField(max_length=10000)
+    Severity = models.CharField(max_length=10000)
+    References = models.CharField(max_length=10000)
