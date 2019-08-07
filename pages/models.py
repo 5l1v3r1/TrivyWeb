ada3de1ffa79e2f83f67af2f862f63dcb5d6d178
KorayAGAYA <koray@korayagaya.com.tr>
2019-08-08 00:22:55 +0300

    Web

--- pages/models.py
+++ pages/models.py
@@ -4,14 +4,22 @@ from django.db import models
 # python manage.py makemigrations pages
 # python manage.py sqlmigrate pages 0001
 # python manage.py migrate
+
+
 # https://belgeler.yazbel.com/python-istihza/Ucuncu_taraf_moduller/django_mod%C3%BCl%C3%BC/django_2.html
 
-class Project(models.Model):
-    VulnerabilityID = models.TextField()
-    PkgName = models.TextField()
-    InstalledVersion = models.TextField()
-    FixedVersion = models.TextField()
-    Title = models.TextField()
-    Description = models.TextField()
-    Severity = models.TextField()
-    References = models.TextField()
+class VulnPages(models.Model):
+
+    class Meta:
+        db_table = 'VulnPages'
+
+    VulnerabilityID = models.TextField(null=True, blank=True,)
+    PkgName = models.TextField(null=True, blank=True,)
+    InstalledVersion = models.TextField(null=True, blank=True,)
+    FixedVersion = models.TextField(null=True, blank=True,)
+    Title = models.TextField(null=True, blank=True,)
+    Description = models.TextField(null=True, blank=True,)
+    Severity = models.TextField(null=True, blank=True,)
+    References = models.TextField(null=True, blank=True,)
+
+
