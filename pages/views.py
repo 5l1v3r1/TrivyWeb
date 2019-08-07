6a96f13a472f3ae9be1274e3712a9abc62419418
KorayAGAYA <koray@korayagaya.com.tr>
2019-08-07 16:44:16 +0300

    Web

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+from django.http import HttpResponse, JsonResponse
+from django.template.loader import render_to_string
+import json
+import pandas as pd
+from pandas.io.json import json_normalize
 
 
 def index(request):
